4ee48a180bbc267ad6afb5164589e50d455b18b9
Retr0 <wang.zhou@hand-china.com>
2022-04-22 10:52:58 +0800

    refactor: event-bus function off

--- src/event-bus.js
+++ src/event-bus.js
@@ -56,25 +56,15 @@ class HYEventBus {
 
   off(eventName, eventCallback) {
     if (typeof eventName !== "string") {
-      throw new TypeError("the event name must be string type")
+      throw new TypeError("the event name must be a string type")
     }
-
     if (typeof eventCallback !== "function") {
-      throw new TypeError("the event callback must be function type")
+      throw new TypeError("the event callback must be a function type")
     }
-
-    const handlers = this.eventBus[eventName]
-    if (handlers && eventCallback) {
-      const newHandlers = [...handlers]
-      for (let i = 0; i < newHandlers.length; i++) {
-        const handler = newHandlers[i]
-        if (handler.eventCallback === eventCallback) {
-          const index = handlers.indexOf(handler)
-          handlers.splice(index, 1)
-        }
-      }
-    }
-
+    const handlers = this.eventBus[eventName] //拿到事件回调数组
+    handlers.forEach((handler, index) => {
+      if (handler.eventCallback == eventCallback) handlers.splice(index, 1) //删除函数
+    })
     if (handlers.length === 0) {
       delete this.eventBus[eventName]
     }
